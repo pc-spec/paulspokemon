5bdc969d834be2988272c8a94af4059931f70959
pc-spec <114579751+pc-spec@users.noreply.github.com>
2023-11-11 17:22:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ pokemons.forEach((pokemon) => {
     });
 });
 
+const pokemons = document.querySelectorAll('li');
+
+pokemons.forEach((pokemon) => {
+    const popup = pokemon.querySelector('.popup');
+    pokemon.addEventListener('mouseenter', () => {
+        const rect = pokemon.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+        
+        // Vérifie s'il y a suffisamment d'espace en dessous
+        if (rect.bottom + popup.clientHeight > windowHeight) {
+            // Si non, affiche le pop-up vers le haut
+            popup.style.bottom = '100%';
+        } else {
+            // Sinon, affiche le pop-up vers le bas
+            popup.style.bottom = '';
+        }
+
+        popup.style.display = 'block';
+    });
+
+    pokemon.addEventListener('mouseleave', () => {
+        popup.style.display = 'none';
+    });
+});
