f0489e19336bca16a3414dbe2384995beca60539
pc-spec <114579751+pc-spec@users.noreply.github.com>
2023-11-24 02:51:34 +0100

    Update mapscript.js

--- mapscript.js
+++ mapscript.js
@@ -1,9 +1,67 @@
-function showInfo(Excelsior) {
-    // Le code pour afficher les informations du Pokémon va ici
-    console.log(Excelsior);
+var pokemonData = [
+    { name: "Excelsior", coords: [1345, 460], description: "Un Pokémon fantastique." },
+    // Ajoute d'autres Pokémon avec leurs coordonnées
+];
+
+// Sélectionne l'élément de la carte et le conteneur des popups
+var map = document.getElementById("map");
+var pokemonContainer = document.getElementById("pokemonContainer");
+
+// Ajoute un gestionnaire d'événements au survol de la carte
+map.addEventListener("mousemove", function (event) {
+    // Récupère les coordonnées de la souris
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+
+    // Itère sur la structure de données pour vérifier si la souris est sur un Pokémon
+    for (var i = 0; i < pokemonData.length; i++) {
+        var pokemon = pokemonData[i];
+
+        if (isMouseOverPokemon(mouseX, mouseY, pokemon.coords)) {
+            // Affiche le popup du Pokémon à la position de la souris
+            showPokemonPopup(pokemon.name, mouseX, mouseY);
+        }
+    }
+
+    // Masque tous les popups s'il n'y a pas de survol
+    hidePokemonPopups();
+});
+
+// Fonction pour vérifier si la souris est sur un Pokémon
+function isMouseOverPokemon(mouseX, mouseY, pokemonCoords) {
+    // Implémente la logique pour vérifier si la souris est sur le Pokémon
+    // Retourne true si c'est le cas, sinon false
 }
 
-function hideInfo() {
-    // Le code pour masquer les informations va ici
-    console.log('Cacher les infos');
+// Fonction pour afficher le popup du Pokémon
+function showPokemonPopup(pokemonName, mouseX, mouseY) {
+    // Crée ou récupère l'élément du popup du Pokémon
+    var pokemonPopup = document.getElementById("popup-" + pokemonName);
+    if (!pokemonPopup) {
+        pokemonPopup = createPokemonPopupElement(pokemonName);
+        pokemonContainer.appendChild(pokemonPopup);
+    }
+
+    // Affiche le popup du Pokémon à la position de la souris
+    pokemonPopup.style.display = "block";
+    pokemonPopup.style.left = mouseX + "px";
+    pokemonPopup.style.top = mouseY + "px";
+}
+
+// Fonction pour masquer tous les popups
+function hidePokemonPopups() {
+    // Itère sur tous les popups et les masque
+    var popups = document.querySelectorAll(".pokemon-popup");
+    for (var i = 0; i < popups.length; i++) {
+        popups[i].style.display = "none";
+    }
+}
+
+// Fonction pour créer l'élément du popup du Pokémon
+function createPokemonPopupElement(pokemonName) {
+    var pokemonPopup = document.createElement("div");
+    pokemonPopup.id = "popup-" + pokemonName;
+    pokemonPopup.className = "pokemon-popup";
+    pokemonPopup.innerText = pokemonName;
+    return pokemonPopup;
 }
