8e26c3a93177bae2a5188662e31d6ac0fd6ac2c1
pc-spec <114579751+pc-spec@users.noreply.github.com>
2023-11-24 02:22:26 +0100

    Create mapscript.js

---
+++ mapscript.js
@@ -0,0 +1,9 @@
+function showInfo(Excelsior) {
+    // Le code pour afficher les informations du Pokémon va ici
+    console.log(Excelsior);
+}
+
+function hideInfo() {
+    // Le code pour masquer les informations va ici
+    console.log('Cacher les infos');
+}
